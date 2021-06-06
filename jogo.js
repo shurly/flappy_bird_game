2232b3f576ef35ba2bde7f00c36e2a844014e9db
Shurly <shurlydomingos@gmail.com>
2021-06-06 15:01:04 -0300

    Almost finished

--- jogo.js
+++ jogo.js
@@ -5,6 +5,9 @@ let frames = 0;
 const som_hit = new Audio();
 som_hit.src = './efeitos/hit.wav';
 
+const som_hit2 = new Audio();
+som_hit2.src = './efeitos/pulo.wav';
+
 const sprites = new Image();
 sprites.src = './sprites.png';
 
@@ -110,9 +113,9 @@ function criaFlappyBird() {
             if (colisão(flappyBird, globais.chao)) {
                 som_hit.play();
 
-                setTimeout(() => {
-                    mudaTela(telas.inicio);
-                }, 500);
+              
+                    mudaTela(telas.gameOver);
+              
 
                 return;
             }
@@ -154,6 +157,133 @@ function criaFlappyBird() {
 }
 
 
+function criaCanos() {
+    const canos = {
+        largura: 52,
+        altura: 400,
+        chao: {
+            spriteX: 0,
+            spriteY: 169,
+        },
+        chao: {
+            spriteX: 0,
+            spriteY: 169,
+        },
+        ceu: {
+            spriteX: 52,
+            spriteY: 169,
+        },
+        espaco: 80,
+        desenha() {
+            canos.pares.forEach(function (par) {
+                const randomY = par.y;
+                const espacoCanos = 90;
+
+                const canoCeuX = par.x;
+                const canoCeuY = randomY;
+
+                //Canos do Céu
+                contexto.drawImage(
+                    sprites,
+                    canos.ceu.spriteX, canos.ceu.spriteY,//Sprite x e sprite y
+                    canos.largura, canos.altura, //tamanho do recorte da imagem do sprite
+                    canoCeuX, canoCeuY,
+                    canos.largura, canos.altura,
+                );
+
+                const canoChaoX = par.x;
+                const canoChaoY = canos.altura + espacoCanos + randomY;
+                //Canos do Chão
+                contexto.drawImage(
+                    sprites,
+                    canos.chao.spriteX, canos.chao.spriteY,//Sprite x e sprite y
+                    canos.largura, canos.altura, //tamanho do recorte da imagem do sprite
+                    canoChaoX, canoChaoY,
+                    canos.largura, canos.altura,
+                )
+                par.canoCeu = {
+                    x: canoCeuX,
+                    y: canos.altura + canoCeuY
+                }
+                par.canoChao = {
+                    x: canoChaoX,
+                    y: canoChaoY
+                }
+            })
+
+        },
+        colidiuFlappy(par) {
+            const cabecaFlappy = globais.flappyBird.y;
+            const peFlappy = globais.flappyBird.y + globais.flappyBird.altura;
+
+            if ((globais.flappyBird.x + globais.flappyBird.largura) >= par.x) {
+                if (cabecaFlappy <= par.canoCeu.y) {
+                    return true;
+                }
+
+                if (peFlappy >= par.canoChao.y) {
+                    return true;
+                }
+
+            }
+
+
+            return false;
+        },
+        pares: [],
+        atualiza() {
+            const passou100Frames = frames % 100 === 0;
+            if (passou100Frames) {
+                canos.pares.push({
+                    x: canvas.width,
+                    y: -150 * (Math.random() + 1),
+                });
+            }
+
+            canos.pares.forEach(function (par) {
+                par.x = par.x - 2;
+
+                if (canos.colidiuFlappy(par)) {
+                    som_hit.play();
+                    mudaTela(telas.gameOver);
+                    
+                }
+
+                if (par.x + canos.largura <= 0) {
+                    canos.pares.shift();
+                }
+            });
+
+        },
+    }
+    return canos;
+}
+
+
+function criaPlacar() {
+    const placar = {
+        pontuacao: 0,
+        desenha() {
+            contexto.font = '35px "VT323"';
+            contexto.textAlign = 'right';
+            contexto.fillStyle = 'white';
+            contexto.fillText(`${placar.pontuacao}`, canvas.width - 10, 35);
+
+        },
+        atualiza() {
+            const intervaloFrames = 10;
+            const passouIntervalo = frames % intervaloFrames === 0;
+
+            if (passouIntervalo) {
+                placar.pontuacao = placar.pontuacao + 1;
+            }
+
+        },
+    }
+
+    return placar;
+}
+
 
 //Tela de inicio
 const mensagemInicio = {
@@ -171,6 +301,31 @@ const mensagemInicio = {
             mensagemInicio.x, mensagemInicio.y,
             mensagemInicio.largura, mensagemInicio.altura,
         );
+    }
+}
+
+//Tela de Game Over
+const mensagemGameOver = {
+    spriteX: 134,
+    spriteY: 153,
+    largura: 226,
+    altura: 200,
+    x: (canvas.width / 2) - 226 / 2,
+    y: 50,
+    pontuacao: 0,
+    desenha() {
+           
+        contexto.drawImage(
+            sprites,
+            mensagemGameOver.spriteX, mensagemGameOver.spriteY,//Sprite x e sprite y
+            mensagemGameOver.largura, mensagemGameOver.altura, //tamanho do recorte da imagem do sprite
+            mensagemGameOver.x, mensagemGameOver.y,
+            mensagemGameOver.largura, mensagemGameOver.altura,
+        );
+        contexto.font = '35px "VT323"';
+        contexto.textAlign = 'right';
+        contexto.fillStyle = 'black';
+        contexto.fillText(`${globais.placar.pontuacao}`, canvas.width - 70, 150);
     }
 }
 
@@ -193,11 +348,14 @@ const telas = {
         inicializa() {
             globais.flappyBird = criaFlappyBird();
             globais.chao = criaChao();
+            globais.canos = criaCanos();
         },
         desenha() {
             planoFundo.desenha(); //Seguir a ordem de desenho para um não sobressair o outro
             globais.flappyBird.desenha();
+
             globais.chao.desenha();
+
             mensagemInicio.desenha();
         },
         click() {
@@ -211,19 +369,40 @@ const telas = {
 };
 
 telas.jogo = {
+    inicializa() {
+        globais.placar = criaPlacar();
+    },
     desenha() {
         planoFundo.desenha(); //Seguir a ordem de desenho para um não sobressair o outro
-        globais.flappyBird.desenha();
+        globais.canos.desenha();
         globais.chao.desenha();
+        globais.flappyBird.desenha();
+        globais.placar.desenha();
     },
     click() {
+        som_hit2.play();
         globais.flappyBird.pula();
     },
     atualiza() {
-        globais.flappyBird.atualiza();
+        globais.canos.atualiza();
         globais.chao.atualiza();
+        globais.flappyBird.atualiza();
+        globais.placar.atualiza();
 
     }
+};
+
+//Game Over
+telas.gameOver = {
+    desenha() {
+        mensagemGameOver.desenha();
+    },
+    atualiza() {
+
+    },
+    click() {
+        mudaTela(telas.inicio);
+    },
 }
 
 
